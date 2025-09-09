2689a17d5f7cc7cdebfb876e6bc65c913cb037f5
ArmaanBhuiyan <armaanb5@nycstudents.net>
2025-09-09 11:44:35 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("What's your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello," + name + "!");
+
     }
 }
+
+
