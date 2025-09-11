219a9f1ea754938ee989b5c9c64eee9bab7513bc
ArmaanBhuiyan <armaanb5@nycstudents.net>
2025-09-11 11:22:55 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException
     {
-        System.out.println("Hello, World!");
         System.out.println("What's your name?");
         Scanner s = new Scanner(System.in);
         String name = s.nextLine();
-        System.out.println("Hello," + name + "!");
+        System.out.println("Hello, " + name + "!");
+        File f = new File("Class.txt");
+        s = new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
 
     }
 }
